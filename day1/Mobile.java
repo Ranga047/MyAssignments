14da1753189f4a2f5fe5f4c7352030d630ee75b3
Ranga047 <124513213+Ranga047@users.noreply.github.com>
2023-02-05 21:27:50 +0530

    Add files via upload
    DAY1

---
+++ day1/Mobile.java
@@ -0,0 +1,22 @@
+package week1.day1;
+
+public class Mobile {
+	public static void main(String[] args) {
+		String mobileName = "Realme";
+		char mobileLogo = 'R';
+		short noOfMobilePiece = 5;
+		int modelNumber = 47;
+		float mobilePrice = 20500;
+		long mobileNumber = 9875456521L;
+		boolean isDamaged = false;
+		
+		System.out.println(mobileName);
+		System.out.println(mobileLogo);
+		System.out.println(noOfMobilePiece);
+		System.out.println(modelNumber);
+		System.out.println(mobilePrice);
+		System.out.println(mobileNumber);
+		System.out.println(isDamaged);
+	}
+
+}
---
+++ day1/TwoWheeler.java
@@ -0,0 +1,18 @@
+package week1.day1;
+
+public class TwoWheeler {
+	public static void main(String[] args) {
+		int noOfWheels = 2;
+		long registerNumber = 5858758995L;
+		boolean isPunctured = true;
+		String bikeName = "Pulsar";
+		double runningKm = 14500.45;
+		
+		System.out.println(noOfWheels);
+		System.out.println(registerNumber);
+		System.out.println(isPunctured);
+		System.out.println(bikeName);
+		System.out.println(runningKm);
+	}
+
+}
