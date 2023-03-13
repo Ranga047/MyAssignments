2ad0253008cb4ff697d07659082475d2e91ccbfe
Ranga047 <124513213+Ranga047@users.noreply.github.com>
2023-03-13 09:40:16 +0530

    Add files via upload

---
+++ Car.java
@@ -0,0 +1,30 @@
+package week1.day2;
+
+public class Car {
+	public int getCarNumber () {
+		return 0711;
+	}
+	private String getCarOwnerName () {
+		return "Ranga";
+	}
+	void printCarName() {
+		System.out.println("Duster");
+	}
+	public boolean isCarPunctured() {
+		return true;
+	}
+	public int subtractTwoNumbers(int num1, int num2) {
+		return num1-num2;
+	}
+	public int multiplyThreeNumbers(int num1, int num2, int num3) {
+		return num1*num2*num3;
+	}
+	public int divideTwoNumbers(int num1, int num2) {
+		return num1/num2;
+	}
+	public static void main(String[] args) {
+		
+	}
+
+
+}
